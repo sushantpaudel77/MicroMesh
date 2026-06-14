3f62673cca9d984735c693533d96741e58dbb10a
Sushant <sushantpaudel77@gmail.com>
2026-06-14 18:40:28 +0545

    feat: change in pipeline

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "main" {
-  filename         = "${path.module}/shipping-processor.zip"
-  function_name    = "${var.project_name}-shipping-processor-${var.environment}"
-  role             = var.lambda_role_arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.13"
-  timeout          = 30
-  memory_size      = 256
+  filename      = "${path.module}/shipping-processor.zip"
+  function_name = "${var.project_name}-shipping-processor-${var.environment}"
+  role          = var.lambda_role_arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.13"
+  timeout       = 30
+  memory_size   = 256
 
   vpc_config {
     subnet_ids         = var.subnet_ids
@@ -25,4 +25,8 @@ resource "aws_lambda_event_source_mapping" "sqs" {
   event_source_arn = var.sqs_queue_arn
   function_name    = aws_lambda_function.main.arn
   batch_size       = 10
+
+  lifecycle {
+    ignore_changes = [event_source_arn]
+  }
 }
